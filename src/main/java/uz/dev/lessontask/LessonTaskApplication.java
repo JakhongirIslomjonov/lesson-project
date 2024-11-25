119653602069d79f67a97b98367e98de559cc783
jaxongir <jahongirnamti@gmail.com>
2024-11-25 15:51:11 +0500

    empty project commit

---
+++ src/main/java/uz/dev/lessontask/LessonTaskApplication.java
@@ -0,0 +1,13 @@
+package uz.dev.lessontask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/dev/lessontask/LessonTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dev.lessontask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
